db6ffbacd9081575d5eab25df474076b971c5b7c
verdantpath <m_dittmann@yahoo.com>
2024-01-14 14:58:22 -0800

    Add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="bg-white shadow h-16 flex justify-between items-stretch">
+      <router-link :to="{name: 'home'}" class="inline-flex items-center px-5 h-full">
+        Home
+      </router-link>
+      <div class="flex items-center gap-1">
+        <router-link :to="{name: 'byName'}" class="inline-flex items-center px-2 h-full hover:bg-purple-200">
+          Search Meals
+        </router-link>
+        <router-link :to="{name: 'byLetter'}" class="inline-flex items-center px-2 h-full hover:bg-purple-200">
+          Search By Letter
+        </router-link>
+        <router-link :to="{name: 'byIngredient'}" class="inline-flex items-center px-2 h-full hover:bg-purple-200">
+          Meals By Ingredient
+        </router-link>
+      </div>
+    </header>
+</template>
+<script>
+  export default {
+    
+  }
+</script>
+<style>
+
+</style>
