58c73cb0684860718802720d5de4997d25a82ff7
verdantpath <m_dittmann@yahoo.com>
2024-01-16 17:51:41 -0800

    Add meal item component

---
+++ src/components/MealItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-white shadow rounded-xl">
+      <router-link :to="{name: 'mealDetails', params: {id: meal.idMeal}}">
+        <img :src="meal.strMealThumb" alt="meal.strMeal" class="rounded-t-xl h-48 w-full object-cover">
+      </router-link>
+      <div class="p-3">
+        <h3 class="font-bold">{{ meal.strMeal }}</h3>
+      <p class="mb-4">Ipsum eiusmod duis aliquip non eu. Id irure dolore nulla minim aliquip. Sint ea qui ex eu duis in et magna ut.</p>
+      <div class="flex items-center justify-between">
+        <YouTubeButton :href="meal.strYoutube">YouTube</YouTubeButton>
+      </div>
+      </div>
+    </div>
+</template>
+<script setup>
+  import YouTubeButton from './YouTubeButton.vue';
+  const { meal } = defineProps({
+    meal: {
+      required: true,
+      type: Object
+    }
+  })
+</script>
+<style>
+
+</style>
