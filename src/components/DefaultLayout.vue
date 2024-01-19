fc66b29f43d67507f0bbd28331fe17cd0efb9e93
verdantpath <m_dittmann@yahoo.com>
2024-01-18 18:02:27 -0800

    Update default layout

--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -2,7 +2,9 @@
   <div class="bg-gray-100 h-full">
     <navbar />
     <main>
-      <router-view />
+      <div class="max-w-[1200px] mx-auto">
+        <router-view />
+      </div>
     </main>
   </div>
 </template>
