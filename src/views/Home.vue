0f9092c12daf21cea81b5395602e3ded4102577f
verdantpath <m_dittmann@yahoo.com>
2024-01-21 10:34:50 -0800

    Add home view

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col p-8">
+    
+  
+  </div>
+</template>
+<script setup>
+  import { computed, onMounted, ref } from 'vue';
+  import store from "../store";
+  import axiosClient from '../axiosClient.js'
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  const ingredients = ref([])
+
+  onMounted(async() => {
+    const response = await axiosClient.get('/list.php?i=list')
+      console.log(response.data)
+      ingredients.value = response.data
+  })
+
+</script>
