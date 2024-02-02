b3fec5d29bd2630fa50df3e748524e1d06092762
verdantpath <m_dittmann@yahoo.com>
2024-02-01 20:09:32 -0800

    Update default layout view

--- src/components/DefaultLayout.vue
+++ src/components/DefaultLayout.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="bg-gray-100 h-full">
     <navbar />
     <main>
       <div class="max-w-[1200px] mx-auto">
         <router-view />
       </div>
     </main>
-  </div>
 </template>
 <script setup>
   import Navbar from '../components/Navbar.vue'
