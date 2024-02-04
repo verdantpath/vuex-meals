02610f51d850f74e36a53f733dfe99f4393eee33
verdantpath <m_dittmann@yahoo.com>
2024-02-03 19:38:29 -0800

    Add meals by ingredient view

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -1,22 +1,17 @@
 <template>
-  <div class="p-8">
-    <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
-    <router-link :to="{name: 'byIngredient', params: {ingredient: ingredient.idIngredient}}" v-for="ingredient of ingredients" :key="ingredient.idIngredient" class="block bg-white rounded p-3 mb-3 shadow">
-      <h3 class="text-2xl font-bold mb-2">{{ ingredient.strIngredient }}</h3>
-      <p>{{ ingredient.strDescription }}</p>
-    </router-link>
-  </div>
+  <Meals :meals="meals" />
 </template>
 <script setup>
+import { computed } from "@vue/reactivity";
 import { onMounted, ref } from 'vue';
-import axiosClient from '../axiosClient';
+import store from "../store";
+import { useRoute } from "vue-router";
+import Meals from "../components/Meals.vue"
 
-const ingredients = ref([])
+const route = useRoute();
+const meals = computed(() => store.state.mealsByIngredient)
 
 onMounted(() => {
-  axiosClient.get('list.php?i=list')
-    .then(({ data }) => {
-      ingredients.value = data.meals
-    })
-})
+    store.dispatch('searchMealsByIngredient', route.params.ingredient);
+  });
 </script>
