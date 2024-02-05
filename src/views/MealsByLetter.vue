834b70b5dce592b54c5ab9c63323f26518d4c041
verdantpath <m_dittmann@yahoo.com>
2024-02-04 16:09:08 -0800

    Add Meals by Letter view

--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -8,9 +8,7 @@
       {{ letter }}
     </router-link>
   </div>
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-    <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
-  </div>
+  <Meals :meals="meals" />
 </template>
 <script setup>
   import { computed } from '@vue/reactivity';
@@ -18,6 +16,7 @@
   import { useRoute } from 'vue-router';
   import store from '../store';
   import MealItem from '../components/MealItem.vue';
+  import Meals from '../components/Meals.vue';
 
   const route = useRoute();
   const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
