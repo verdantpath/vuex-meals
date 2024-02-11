4eaded052b6c7103cd7f238799ddefd22058d2f7
verdantpath <m_dittmann@yahoo.com>
2024-02-11 12:27:46 -0800

    Update router

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import MealsByIngredient from '../views/MealsByIngredient.vue'
 import MealsByName from '../views/MealsByName.vue'
 import MealsByLetter from '../views/MealsByLetter.vue'
 import MealDetails from '../views/MealDetails.vue'
+import Ingredients from '../views/Ingredients.vue'
 
 const routes = [
   {
@@ -28,7 +29,12 @@ const routes = [
         component: MealsByLetter
       },
       {
-        path: '/by-ingredient/:ingredient?',
+        path: 'ingredients',
+        name: 'ingredients',
+        component: Ingredients
+      },
+      {
+        path: '/by-ingredient/:ingredient',
         name: 'byIngredient',
         component: MealsByIngredient
       },
