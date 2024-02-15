913396985329e33268fecd29deda9bec1708477d
verdantpath <m_dittmann@yahoo.com>
2024-02-14 20:01:42 -0800

    Update meal item component

--- src/components/MealItem.vue
+++ src/components/MealItem.vue
@@ -1,16 +1,25 @@
 <template>
-  <div class="bg-white shadow rounded-xl">
-      <router-link :to="{name: 'mealDetails', params: {id: meal.idMeal}}">
-        <img :src="meal.strMealThumb" alt="meal.strMeal" class="rounded-t-xl h-48 w-full object-cover">
-      </router-link>
-      <div class="p-3">
-        <h3 class="font-bold">{{ meal.strMeal }}</h3>
-      <p class="mb-4">Ipsum eiusmod duis aliquip non eu. Id irure dolore nulla minim aliquip. Sint ea qui ex eu duis in et magna ut.</p>
+  <div class="bg-white shadow rounded-xl hover:scale-105 transition-all">
+    <router-link :to="{name: 'mealDetails', params: {id: meal.idMeal}}">
+      <img :src="meal.strMealThumb" alt="meal.strMeal" class="rounded-t-xl h-48 w-full object-cover">
+    </router-link>
+    <div class="p-3">
+      <h3 class="font-bold">{{ meal.strMeal }}</h3>
+      <!-- <p class="mb-4">
+        {{ $filters.truncateWords(meal.strInstructions, 20) }}
+      </p> -->
+      <p class="mb-4">
+        {{ (meal.strInstructions || "").split(" ").slice(0, 20).join(" ") }}
+      </p>
+      <p class="mb-4">
+        Lorem ipsum bitch
+      </p>
+      <!-- <pre>{{ meal }}</pre> -->
       <div class="flex items-center justify-between">
-        <YouTubeButton :href="meal.strYoutube">YouTube</YouTubeButton>
-      </div>
+        <YouTubeButton :href="meal.strYoutube" />
       </div>
     </div>
+  </div>
 </template>
 <script setup>
   import YouTubeButton from './YouTubeButton.vue';
