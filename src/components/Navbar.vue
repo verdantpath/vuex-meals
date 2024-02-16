509739f32f9a349f99cecf70a5b2551ff4d5be78
verdantpath <m_dittmann@yahoo.com>
2024-02-15 23:17:38 -0800

    Update navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,16 @@
 <template>
   <header class="bg-white shadow h-16 flex justify-between items-stretch">
-      <router-link :to="{name: 'home'}" class="inline-flex items-center px-5 h-full">
+      <router-link :to="{name: 'home'}" class="inline-flex items-center px-5 h-full text-orange-500 font-bold">
         Home
       </router-link>
       <div class="flex items-center gap-1">
-        <router-link :to="{name: 'byName'}" class="inline-flex items-center px-2 h-full hover:bg-purple-200">
+        <router-link :to="{name: 'byName'}" class="inline-flex items-center px-2 h-full hover:bg-orange-500 hover:text-white">
           Search Meals
         </router-link>
-        <router-link :to="{name: 'byLetter'}" class="inline-flex items-center px-2 h-full hover:bg-purple-200">
+        <router-link :to="{name: 'byLetter'}" class="inline-flex items-center px-2 h-full hover:bg-orange-500 hover:text-white">
           Search By Letter
         </router-link>
-        <router-link :to="{name: 'ingredients'}" class="inline-flex items-center px-2 h-full hover:bg-purple-200">
+        <router-link :to="{name: 'ingredients'}" class="inline-flex items-center px-2 h-full hover:bg-orange-500 hover:text-white">
           Meals By Ingredient
         </router-link>
       </div>
