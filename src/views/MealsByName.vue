471970aa32c93bfb6a2e9a6addd2ccdcf0037b08
verdantpath <m_dittmann@yahoo.com>
2024-02-21 16:31:19 -0800

    Update meals by name view

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="p-8 pb-0">
+    <h1 class="text-4xl font-bold mb-4 text-orange-500">Search Meals by Name</h1>
+  </div>
+  <div class="px-8 pb-3">
     <input 
       type="text" 
       v-model="keyword"
-      class="rounded border-2 border-gray-200 w-full" 
+      class="rounded border-2 border-gray-200 focus:ring-orange-500 focus:border-orange-500 w-full" 
       placeholder="Search for Meals" 
       @change="searchMeals"
     />
   </div>
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-    <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
-  </div>
+  <Meals :meals="meals" />
 </template>
 <script setup>
 import { ref, onMounted } from 'vue';
@@ -19,6 +20,7 @@ import store from '../store';
 import { useRouter, useRoute } from 'vue-router';
 import YouTubeButton from "../components/YouTubeButton.vue"
 import MealItem from "../components/MealItem.vue";
+import Meals from "../components/Meals.vue"
 
 const router = useRouter();
 const route = useRoute();
@@ -26,8 +28,13 @@ const keyword = ref('');
 const meals = computed(() => store.state.searchedMeals)
 
 function searchMeals() {
-  store.dispatch('searchMeals', keyword.value)
-  router.push({ path: `/by-name/${keyword.value}` })
+  if (keyword.value) {
+    store.dispatch('searchMeals', keyword.value)
+    router.push({ path: `/by-name/${keyword.value}` })
+  } else {
+    store.commit("setSearchedMeals", []);
+    router.push({ path: '/by-name'})
+  }
 }
 
 onMounted(() => {
